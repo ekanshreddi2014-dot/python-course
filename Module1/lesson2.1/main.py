f67fa403527995a4442d5ab09a51cecdd6b71980
ekanshreddi2014-dot <ekanshreddi2014@gmail.com>
2026-07-05 14:36:03 +0530

    my project 2

---
+++ Module1/lesson2.1/main.py
@@ -0,0 +1,19 @@
+a = input("birthday 1 date and who's birthday is it : ")
+b = input("birthday 2 date and who's birthday is it : ")
+c = input("birthday 3 date and who's birthday is it : ")
+d = input("birthday 4 date and who's birthday is it : ")
+e = input("birthday 5 date and who's birthday is it : ")
+print("what birthday do you want to know about?")
+birthday = input()
+if birthday == 'birthday 1':
+    print(a)
+elif birthday == 'birthday 2':
+    print(b)
+elif birthday == 'birthday 3':
+    print(c)
+elif birthday == 'birthday 4':
+    print(d)
+elif birthday == 'birthday 2':
+    print(e)
+else:
+    print("I dont have information about that.")  
